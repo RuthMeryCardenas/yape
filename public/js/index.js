da7b414c7d952dd1a9bea10066e0bc33f9307109
RuthMery <ruth.cardenas.perez@gmail.com>
2017-06-21 12:17:49 -0500

    index.js-Textos para la página web

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,10 @@ const render = (current_screen) => {
       wrapper.append(Welcome());
       break;
     case "Register_phone":
-      // console.log("next screen");
-      // nuevo componente a pintarse
+      wrapper.append(Register_phone());
       break;
     default:
-      wrapper.append(Welcome());
+    wrapper.append(Welcome());
   }
 
   root.append(wrapper);
@@ -22,6 +21,13 @@ const render = (current_screen) => {
 
 const state = {
   current_screen : null
+};
+
+const texts = {
+  titles : ["Paga a tus amigos"],
+  messages : ["Paga a quien quieras desde donde quieras, sin usar efectivo"],
+  subtitles : ["Para comenzar validaremos tu número"],
+  instructions : ["Recibirás un SMS con un código de validación"]
 };
 
 $( _ => {
